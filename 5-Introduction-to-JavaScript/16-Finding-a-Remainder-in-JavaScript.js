ce47743d3009eaaf88dafbd7cd62c95d4f215bdb
Deanna Swallow <swallowdeanna@gmail.com>
2019-04-12 15:20:20 -0600

    Add solutions for JS Ex 16, 17, 18, 19, and 20

---
+++ 5-Introduction-to-JavaScript/16-Finding-a-Remainder-in-JavaScript.js
@@ -0,0 +1,6 @@
+// Learned that % is the remainder operator, not the modulus operator. This
+// is because % doesn't work properly with negative numbers.
+
+// Only change code below this line
+
+var remainder = 11 % 3;
---
+++ 5-Introduction-to-JavaScript/17-Compound-Assignment-With-Augmented-Addition.js
@@ -0,0 +1,12 @@
+// You can use a compound statement to do an assignment and mathematical
+// operation in one step.
+
+var a = 3;
+var b = 17;
+var c = 12;
+
+// Only modify code below this line
+
+a += 12;
+b += 9;
+c += 7;
