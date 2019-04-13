3189ac738dd50dd9a50cbeb9ede7d317e143fd29
Deanna Swallow <swallowdeanna@gmail.com>
2019-04-12 20:05:22 -0600

    Add solution for JS Ex 45

---
+++ 5-Introduction-to-JavaScript/45-Shopping_List.js
@@ -0,0 +1,8 @@
+// Practice adding items to an array.
+
+var myList = [];
+myList.push(["cookies", 12]);
+myList.push(["apples", 6]);
+myList.push(["toothbrushes", 2]);
+myList.push(["bananas", 5]);
+myList.push(["creamer", 1]);
