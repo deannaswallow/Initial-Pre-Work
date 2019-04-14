3872ece93f39d3ebb65b6dd4084218b75e0f8a67
Deanna Swallow <swallowdeanna@gmail.com>
2019-04-14 15:43:49 -0600

    Add solutions for JS Ex 81, 82, 83, 84, 85, and 86

---
+++ 5-Introduction-to-JavaScript/81-Accessing_Object_Properties_with_Dot_Notation.js
@@ -0,0 +1,15 @@
+// Two main ways of accessing info in an object: dot notation and bracket
+// notation. Use dot notation if you know the name of the property you're trying
+// to access.
+
+// Setup
+var testObj = {
+  "hat": "ballcap",
+  "shirt": "jersey",
+  "shoes": "cleats"
+};
+
+// Only change code below this line
+
+var hatValue = testObj.hat;      // Change this line
+var shirtValue = testObj.shirt;    // Change this line
---
+++ 5-Introduction-to-JavaScript/82-Accessing_Object_Properties_with_Bracket_Notation.js
@@ -0,0 +1,13 @@
+// You will need to use bracket notation if the property name has a space in it.
+
+// Setup
+var testObj = {
+  "an entree": "hamburger",
+  "my side": "veggies",
+  "the drink": "water"
+};
+
+// Only change code below this line
+
+var entreeValue = testObj["an entree"];   // Change this line
+var drinkValue = testObj["the drink"];    // Change this line
