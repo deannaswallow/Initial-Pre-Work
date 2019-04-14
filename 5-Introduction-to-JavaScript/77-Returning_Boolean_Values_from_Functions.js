f6783efd4fc7e30f4e846dda68c788a191f7b03c
Deanna Swallow <swallowdeanna@gmail.com>
2019-04-14 14:48:12 -0600

    Add solutions for JS Ex 77, 78, 79, and 80

---
+++ 5-Introduction-to-JavaScript/77-Returning_Boolean_Values_from_Functions.js
@@ -0,0 +1,7 @@
+// There is a more succinct way to return true or false from a function.
+
+function isLess(a, b) {
+  return a < b;
+}
+// Change these values to test
+isLess(10, 15);
---
+++ 5-Introduction-to-JavaScript/80-Build_JavaScript_Objects.js
@@ -0,0 +1,18 @@
+// Reviewed object structure. All properties will be typecast as strings.
+
+// Example
+var ourDog = {
+  "name": "Camper",
+  "legs": 4,
+  "tails": 1,
+  "friends": ["everything!"]
+};
+
+// Only change code below this line.
+
+var myDog = {
+  name: "Gizmo",
+  legs: 4,
+  tails: 0,
+  friends: ["Lucky", "Max", "Luna"]
+};
